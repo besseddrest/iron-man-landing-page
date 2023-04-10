1e91d30a1ca669e49af04cd662766de0078996ae
Harold C <hcabalic@gmail.com>
2023-04-09 23:50:41 -0700

    add responsiveness to hero

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,14 +14,14 @@ export default function Home() {
         <title>Iron Man Landing Page</title>
       </Head>
       <section className="hero">
-        <div className="columns">
+        <div className="columns is-flex">
           <div className="column">
             <p className="hero__description">
               API Wounded, captured and forced to build a weapon by his enemies, billionaire industrialist Tony Stark instead created an advanced suit of armor to save his life and escape captivity. Now with a new outlook on life, Tony uses his money and intelligence to make the world a safer, better place as Iron Man.
             </p>
             <button className="button hero__button">Get Started</button>
           </div>
-          <div className="column">
+          <div className="column is-flex-shrink-1">
             <Image src="/images/hero-iron-man.png" width="410" height="559" alt="Iron Man Cartoon" />
           </div>
         </div>
