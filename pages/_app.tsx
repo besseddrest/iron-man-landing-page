3f0b4056cfe35720bc50d33e1b6252958fca99e8
Harold C <hcabalic@gmail.com>
2023-04-11 02:54:06 -0700

    Add styles for profile section

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
 import { AppProps } from 'next/app';
 import '../styles/global.scss'
 import '../styles/iron-man-styles.scss'
+import localFont from 'next/font/local'
+
+const nunito = localFont({
+  src: [
+    {
+      path: '../public/fonts/Nunito-Regular.ttf',
+      weight: '400'
+    }
+  ]
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
--- pages/index.tsx
+++ pages/index.tsx
@@ -16,6 +16,7 @@ export default function Home() {
       <section className="hero">
         <div className="columns is-flex">
           <div className="column">
+          { /* ToDo: populate hero__description via API response */ }
             <p className="hero__description">
               API Wounded, captured and forced to build a weapon by his enemies, billionaire industrialist Tony Stark instead created an advanced suit of armor to save his life and escape captivity. Now with a new outlook on life, Tony uses his money and intelligence to make the world a safer, better place as Iron Man.
             </p>
@@ -27,7 +28,43 @@ export default function Home() {
         </div>
       </section>
       <section className="profile">
-        <h2>Profile Section</h2>
+        <h2 className="profile__header">Contrary to popular belief, he knows exactly what he’s doing.</h2>
+        <div className="profile__content columns is-flex">
+          <div className="profile__image column is-half-tablet">
+            <Image src="/images/Iron Man API Asset.png" width="417" height="417" alt="API asset" />
+          </div>
+          <div className="profile__attributes column columns is-flex is-half-tablet">
+            { /* ToDo: populate via API response, add icons */ }
+            <div className="profile__attribute column">
+              <Image className="profile__attribute-icon" src="/images/icon-intelligence.png" width="53" height="55" alt="Iron Man: Intelligence" />
+              <h3 className="profile__attribute-name">Super-Genius Intelligence</h3>
+              <p className="profile__attribute-desc">
+                Tony Stark is far more than a mechanical engineering prodigy who graduated from the Massachusetts Institute of Technology with honors at the age of 17.
+              </p>
+            </div>
+            <div className="profile__attribute column">
+              <Image className="profile__attribute-icon" src="/images/icon-intelligence.png" width="53" height="55" alt="Iron Man: Intelligence" />
+              <h3 className="profile__attribute-name">Super-Genius Intelligence</h3>
+              <p className="profile__attribute-desc">
+                Tony Stark is far more than a mechanical engineering prodigy who graduated from the Massachusetts Institute of Technology with honors at the age of 17.
+              </p>
+            </div>
+            <div className="profile__attribute column">
+              <Image className="profile__attribute-icon" src="/images/icon-intelligence.png" width="53" height="55" alt="Iron Man: Intelligence" />
+              <h3 className="profile__attribute-name">Super-Genius Intelligence</h3>
+              <p className="profile__attribute-desc">
+                Tony Stark is far more than a mechanical engineering prodigy who graduated from the Massachusetts Institute of Technology with honors at the age of 17.
+              </p>
+            </div>
+            <div className="profile__attribute column">
+              <Image className="profile__attribute-icon" src="/images/icon-intelligence.png" width="53" height="55" alt="Iron Man: Intelligence" />
+              <h3 className="profile__attribute-name">Super-Genius Intelligence</h3>
+              <p className="profile__attribute-desc">
+                Tony Stark is far more than a mechanical engineering prodigy who graduated from the Massachusetts Institute of Technology with honors at the age of 17.
+              </p>
+            </div>
+          </div>
+        </div>
       </section>
       <section className="carousel">
         <p>image carousel goes here</p>
