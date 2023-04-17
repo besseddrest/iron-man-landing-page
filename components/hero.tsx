8bbe4bffd33c297a31cc20e6746b202d1256a858
Harold C <hcabalic@gmail.com>
2023-04-16 23:49:13 -0700

    get hero description fr API

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,13 +1,15 @@
 import Image from 'next/image';
 
-export default function Hero() {
+export default function Hero(props: any) {
+  const { heroDesc } = props;
+
   return (
     <section className="hero">
       <div className="columns is-flex">
         <div className="column">
         { /* ToDo: populate hero__description via API response */ }
           <p className="hero__description">
-            API Wounded, captured and forced to build a weapon by his enemies, billionaire industrialist Tony Stark instead created an advanced suit of armor to save his life and escape captivity. Now with a new outlook on life, Tony uses his money and intelligence to make the world a safer, better place as Iron Man.
+            { heroDesc }
           </p>
           <button className="button hero__button">Get Started</button>
         </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,14 +6,34 @@ import { GetStaticProps } from 'next'
 import Hero from '../components/hero'
 import Profile from '../components/profile'
 import Comics from '../components/comics'
+import { createHash } from 'crypto'
+import { useEffect, useState } from 'react'
 
 export default function Home() {
+  const [heroDesc, setHeroDesc] = useState('');
+
+  useEffect(() => {
+    getHeroDescription();
+  }, []);
+
+  async function getHeroDescription() {
+    const time = new Date();
+    const pub = process.env.NEXT_PUBLIC_PUBLIC_API_KEY;
+    const pri = process.env.NEXT_PUBLIC_PRIVATE_API_KEY;
+    const hash = createHash('md5')
+      .update(time + pri + pub)
+      .digest('hex');
+    const hero = await fetch(`https://gateway.marvel.com/v1/public/characters/1009368?ts=${time}&apikey=${pub}&hash=${hash}`)
+      .then(response => response.json())
+      .then(resp => setHeroDesc(resp.data.results[0].description));
+  }
+
   return (
     <Layout home>
       <Head>
         <title>Iron Man Landing Page</title>
       </Head>
-      <Hero />
+      <Hero heroDesc={heroDesc} />
       <Profile />
       <Comics />
     </Layout>
